f26b82d2c76f190d968fd6f54e6ce0ee89d01531
Romulo <romulo@psu.edu>
2024-09-20 10:18:13 -0400

    Adding new container option

--- Ex2-Pub-Launch/ex2-recipe/packages/__solution__/main.py
+++
@@ -1,7 +0,0 @@
-def main():
-    # TODO: implement call to exercise solution from the `meat` directory here
-    print("\nERROR: Exercise `__solution__` was not implemented.\n")
-
-
-if __name__ == "__main__":
-    main()
